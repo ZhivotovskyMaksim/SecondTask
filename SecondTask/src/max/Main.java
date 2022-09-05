302f59b6074d7054040bc14ae11dc2559e2fe49e
Maksim Zhivotovsky <Maksim Zhivotovsky@DESKTOP-9D3196E>
2022-09-06 01:58:33 +0500

    Task1 Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени

---
+++ SecondTask/src/max/Main.java
@@ -0,0 +1,69 @@
+package max;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Main {
+	
+    static class Person {
+        final int id;
+
+        final String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return getId() == person.getId() && getName().equals(person.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName());
+        }
+        
+        public String toString() {
+        	return name;
+        }
+    }
+
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+    };
+
+	public static void main(String[] args) {
+		Map<String, Long> map = Arrays.asList(RAW_DATA).stream()
+				.distinct()
+				.collect(Collectors.groupingBy(Person::getName, Collectors.counting()));
+		
+		System.out.println(map);
+
+	}
+
+}
